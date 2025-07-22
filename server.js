b330bd145c8cf927d061a49cf8a1d6593698508e
ThameeraSithnuwan <149369868+ThameeraSithnuwan@users.noreply.github.com>
2025-07-22 12:58:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,9 +2,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+
+app.use((req, res, next) => {
+  console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`)
+  next() 
+})
+
 app.get('/', (req, res) => {
   res.send('Thank You, Thameera! Nilesh')
 })
+
 
 app.get('/hello', (req, res) => {
   res.send('Hi, Thameera! JAnuda Nilesh')
